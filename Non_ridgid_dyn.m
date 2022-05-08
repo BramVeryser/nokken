ccb133156c4a864cf02276514467ff305048f126
Bram Veryser <r0778645@esat.kuleuven.be>
2022-05-08 18:23:10 +0200

    numerieke analyse begin

--- Non_ridgid_dyn.m
+++ Non_ridgid_dyn.m
@@ -3,10 +3,10 @@ clear; close all;
 % Constanten
 cam = load('Geometrie_e.mat');
 %rise pas aan
-rise=1;
+rise=3;
 k_f = 10; 
-k_s = 10;
-m = 1;%18?
+k_s = 0;
+m = 3;%18?
 dzeta = 0.1;
 omega = pi;
 omega_n = sqrt( (k_f+k_s)/m);
@@ -29,13 +29,27 @@ t_1 = 2*pi/180 * beta / omega;
 lambda = t_1/t_n; % lambda is zeer klein <<10 dus benadering niet geldig
 dzeta*lambda - 0.75 %moet groter zijn dan nul anders is benadering slecht
 % het ziet er naar uit dat we numeriek gaan moeten uitwerken 
+% k_f kiezen zodat er aan de benaderingsvoorwaarde voldaan wordt
+k_f = 2*pi*m*.75/t_1/dzeta -k_s
+
 
 theta = cam.S;
-
+theta_dot  = cam.V;
 t= 0:0.001:t_1; % in seconden
-tau =  0:0.001:1;%t/t_1;  %
+tau =  0:(1/8000):1;%t/t_1;  %
 
 A_max = Q/(2*pi*lambda)^N
+
+
+%% numerieke analyse
+teller = (2*pi*lambda)^2;
+noemer = [1, 2*dzeta*(2*pi*lambda), (2*pi*lambda)^2];
+sys = tf(teller, noemer);
+
+theta_3 = theta(200e2:280e2);
+theta_init = [theta(200e2) theta_dot(200e2)];
+lsim(sys,theta_3/30-1,tau, theta_init)
+
 
 
     
